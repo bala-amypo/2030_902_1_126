26a5fd8d3538e15fbdc4fee6b699772f931d6ab7
bala-amypo <bala@amypo.in>
2025-12-19 04:23:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/Repository/CustomerProfileRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+
+import com.example.demo.entity.CustomerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.Optional;
+
+
+public interface CustomerProfileRepository extends JpaRepository<CustomerProfile, Long> {
+Optional<CustomerProfile> findByCustomerId(String customerId);
+Optional<CustomerProfile> findByEmail(String email);
+}
