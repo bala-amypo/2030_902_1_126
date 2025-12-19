5073b9e869a499d10fdc462cb3dcffbecea25099
bala-amypo <bala@amypo.in>
2025-12-19 04:29:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/Repository/PurchaseRecordRespository.java
@@ -0,0 +1,15 @@
+// PurchaseRecordRepository.java
+package com.example.demo.repository;
+
+import com.example.demo.entity.PurchaseRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PurchaseRecordRepository extends JpaRepository<PurchaseRecord, Long> {
+
+    List<PurchaseRecord> findByCustomerId(Long customerId);
+
+    List<PurchaseRecord> findByPurchaseDateBetween(LocalDate start, LocalDate end);
+}
